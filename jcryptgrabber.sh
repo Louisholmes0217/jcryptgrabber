42c3ec6e6fb2db09395b060db84ddd5a9e1e68e7
louisholmes0217 <louisholmes0217@gmail.com>
2024-03-21 18:05:45 +0000

    added folders

--- jcryptgrabber.sh
+++ jcryptgrabber.sh
@@ -13,7 +13,7 @@ tshark -r $1 -R "data and tcp" -2 -T fields -e tcp.stream -e ip.addr -e data > o
 
 python3 outparse.py $datatype
 
-rm out.txt # Remove this line for debugging purposes when testing 1 pcap at a time
+#rm out.txt # Remove this line for debugging purposes when testing 1 pcap at a time
 
 for FILE in streamout/${datatype}*;
 do
